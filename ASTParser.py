723ed2c7e90b687ef7d428a5b965c489e31ff6f8
TobyZT <toby771293912@outlook.com>
2022-03-01 14:53:49 +0800

    2022.3.1 加入了java程序的解析功能

--- ASTParser.py
+++ ASTParser.py
@@ -2,30 +2,33 @@ import sys
 import networkx as nx
 from pycparser import c_parser
 import numpy as np
-import zss
 import re
-
-from test import *
+import javalang
 
 sys.path.extend(['.', '..'])
 
 
 class ASTParser:
     typeNum = 0
-    typeNames = []
+    typeNames = ['ArrayDecl', 'ArrayRef', 'Assignment', 'Alignas', 'BinaryOp', 'Break', 'Case', 'Cast', 'Compound',
+                 'CompoundLiteral', 'Constant', 'Continue', 'Decl', 'DeclList', 'Default', 'DoWhile',
+                 'EllipsisParam', 'EmptyStatement', 'Enum', 'Enumerator', 'EnumeratorList', 'ExprList', 'FileAST',
+                 'For', 'FuncCall', 'FuncDecl', 'FuncDef', 'Goto', 'ID', 'IdentifierType', 'If', 'InitList',
+                 'Label', 'NamedInitializer', 'ParamList', 'PtrDecl', 'Return', 'StaticAssert', 'Struct',
+                 'StructRef', 'Switch', 'TernaryOp', 'TypeDecl', 'Typedef', 'Typename', 'UnaryOp', 'Union', 'While',
+                 'Pragma']
     vector = []
     edge_list = []
     func_list = []
     var_list = []
 
     def __init__(self):
-        self.parser = c_parser.CParser()
-        self.G = nx.Graph()
-        with open("token_type.cfg", "r") as f:
-            lines = f.readlines()
-            # typeNames = ['ArrayDecl', 'ArrayRef', ... ]
-            self.typeNames = [x.rstrip() for x in lines]
-            self.typeNum = len(self.typeNames)
+        self.G = nx.DiGraph()
+        # with open("token_type.cfg", "r") as f:
+        #     lines = f.readlines()
+        #     # typeNames = ['ArrayDecl', 'ArrayRef', ... ]
+        #     self.typeNames = [x.rstrip() for x in lines]
+        self.typeNum = len(self.typeNames)
         self.G.add_nodes_from(self.typeNames)
 
     def traverse(self, root):
@@ -37,14 +40,18 @@ class ASTParser:
 
     def parseCode(self, code):
         # preprocess:
-        code = re.sub('(?<!:)\\/\\/.*|\\/\\*(\\s|.)*?\\*\\/', "", code).strip()
-        ast = self.parser.parse(code)
+        code = re.sub(r'(?<!:)\/\/.*|\/\*(\s|.)*?\*\/', "", code).strip()  # strip comments
+        code = re.sub(r'^#.+$', "", code, flags=re.M).strip()  # strip macro
+
+        parser = c_parser.CParser()
+        ast = parser.parse(code)
 
         # clear results of last parsing
         self.edge_list.clear()
         self.var_list.clear()
         self.func_list.clear()
         self.G.clear_edges()
+        # traver AST
         self.traverse(ast)
 
         # add edges to graph
@@ -58,7 +65,8 @@ class ASTParser:
         res = {}
         res['harmonicCent'] = [cent / len(self.G) if cent > 1e-5 else 0 for cent in
                                nx.harmonic_centrality(self.G).values()]
-        res['eigenvectorCent'] = [cent if cent > 1e-5 else 0 for cent in nx.eigenvector_centrality(self.G).values()]
+        # res['eigenvectorCent'] = [cent if cent > 1e-5 else 0 for cent in nx.eigenvector_centrality(self.G).values()]
+        res['degreeCent'] = [cent if cent > 1e-5 else 0 for cent in nx.degree_centrality(self.G).values()]
         res['closenessCent'] = [cent if cent > 1e-5 else 0 for cent in nx.closeness_centrality(self.G).values()]
         res['betweennessCent'] = [cent if cent > 1e-5 else 0 for cent in nx.betweenness_centrality(self.G).values()]
 
@@ -79,7 +87,7 @@ class ASTParser:
 
     @staticmethod
     def compareAll(vec1, vec2):
-        # row: cosine correlation
+        # column: cosine correlation
         res = []
         for (key, value) in vec1.items():
             row = []
@@ -92,35 +100,78 @@ class ASTParser:
             # 计算相关系数
             correlation = np.corrcoef(v1, v2)
             row.append(correlation[0][1])
-            # # 计算曼哈顿距离
-            # manhattan = sum(abs(v1 - v2)) / len(v1)
-            # row.append(manhattan)
-            # # 计算切比雪夫距离
-            # chebyshev = abs(v1 - v2).max()
-            # row.append(chebyshev)
-            # 结果加入一行
             res.append(row)
         return res
 
 
+class JavaASTParser(ASTParser):
+    typeNames = ["CompilationUnit", "Import", "Documented", "Declaration", "TypeDeclaration", "PackageDeclaration",
+                 "ClassDeclaration", "EnumDeclaration", "InterfaceDeclaration", "AnnotationDeclaration", "Type",
+                 "BasicType", "ReferenceType", "TypeArgument", "TypeParameter", "Annotation", "ElementValuePair",
+                 "ElementArrayValue", "Member", "MethodDeclaration", "FieldDeclaration", "ConstructorDeclaration",
+                 "ConstantDeclaration", "ArrayInitializer", "VariableDeclaration", "LocalVariableDeclaration",
+                 "VariableDeclarator", "FormalParameter", "InferredFormalParameter", "Statement", "IfStatement",
+                 "WhileStatement", "DoStatement", "ForStatement", "AssertStatement", "BreakStatement",
+                 "ContinueStatement", "ReturnStatement", "ThrowStatement", "SynchronizedStatement", "TryStatement",
+                 "SwitchStatement", "BlockStatement", "StatementExpression", "TryResource", "CatchClause",
+                 "CatchClauseParameter", "SwitchStatementCase", "ForControl", "EnhancedForControl", "Expression",
+                 "Assignment", "TernaryExpression", "BinaryOperation", "Cast", "MethodReference", "LambdaExpression",
+                 "Primary", "Literal", "This", "MemberReference", "Invocation", "ExplicitConstructorInvocation",
+                 "SuperConstructorInvocation", "MethodInvocation", "SuperMethodInvocation", "SuperMemberReference",
+                 "ArraySelector", "ClassReference", "VoidClassReference", "Creator", "ArrayCreator", "ClassCreator",
+                 "InnerClassCreator", "EnumBody", "EnumConstantDeclaration", "AnnotationMethod"]
+
+    def traverse(self, root):
+        if isinstance(root, list) and len(root) == 0:
+            return
+
+        if hasattr(root, 'children'):
+            print(root.__class__.__name__)
+            for child in root.children:
+                if hasattr(child, 'children'):
+                    self.edge_list.append([root.__class__.__name__, child.__class__.__name__])
+                self.traverse(child)
+
+        if isinstance(root, list):
+            for child in root:
+                if hasattr(child, 'children'):
+                    self.edge_list.append([root.__class__.__name__, child.__class__.__name__])
+                self.traverse(child)
+
+    def parseCode(self, code):
+        # preprocess:
+        code = re.sub(r'(?<!:)\/\/.*|\/\*(\s|.)*?\*\/', "", code).strip()  # strip comments
+        code = re.sub(r'^#.+$', "", code, flags=re.M).strip()  # strip macro
+
+        tree = javalang.parse.parse(code)
+        self.traverse(tree)
+
+        # add edges to graph
+        for [u, v] in self.edge_list:
+            if self.G.has_edge(u, v):
+                self.G[u][v]['weight'] += 1
+            else:
+                self.G.add_edge(u, v, weight=1)
+
+        # calculate featured vector
+        res = {}
+        res['harmonicCent'] = [cent / len(self.G) if cent > 1e-5 else 0 for cent in
+                               nx.harmonic_centrality(self.G).values()]
+        # res['eigenvectorCent'] = [cent if cent > 1e-5 else 0 for cent in nx.eigenvector_centrality(self.G).values()]
+        res['degreeCent'] = [cent if cent > 1e-5 else 0 for cent in nx.degree_centrality(self.G).values()]
+        res['closenessCent'] = [cent if cent > 1e-5 else 0 for cent in nx.closeness_centrality(self.G).values()]
+        res['betweennessCent'] = [cent if cent > 1e-5 else 0 for cent in nx.betweenness_centrality(self.G).values()]
+
+        # self.vector = [cent if cent > 1e-5 else 0 for cent in v.values()]
+        return res
+
+
 if __name__ == '__main__':
-    p = ASTParser()
-    v1 = p.parseCode(text1)
-    v2 = p.parseCode(text2)
-    v3 = p.parseCode(text3)
-
-    v4 = p.parseCode(code1)
-    v5 = p.parseCode(code2)
-
-    for (key, value) in v4.items():
-        print(key + " similarity 4 - 5: ", p.compare(value, v5[key]))
-    print('----------')
-
-    for (key, value) in v1.items():
-        print(key + " similarity 1 - 2: ", p.compare(value, v2[key]))
-    print('----------')
-    for (key, value) in v1.items():
-        print(key + " similarity 1 - 3: ", p.compare(value, v3[key]))
-    print('-----------')
-    for (key, value) in v2.items():
-        print(key + " similarity 2 - 3: ", p.compare(value, v3[key]))
+    p = JavaASTParser()
+    with open("test.java", "r") as f:
+        res = p.parseCode(f.read())
+    print(res)
+    # p = ASTParser()
+    # with open("Metrics/test.c", "r", encoding='ISO-8859-1') as f:
+    #     res = p.parseCode(f.read())
+    # print(res)

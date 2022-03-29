61c6f5b18078534a761b59c4a354d47df9e10785
TobyZT <toby771293912@outlook.com>
2022-03-30 01:41:58 +0800

    2022.3.30 c++ program parsing now supported

--- test.py
+++ test.py
@@ -1,12 +1,93 @@
 import pycparser.plyparser
 
-from ASTParser import ASTParser
+from ASTParser import ASTParser, CppASTParser
 import numpy as np
 import os
 import math
 import random
 from matplotlib import pyplot as plt
 from sklearn import metrics as sk_metrics
+
+
+def getCppSimilarityGroup():
+    # 自比较
+    path = "ProgramData\\"
+    parser = CppASTParser()
+    log = open("log1.txt", "w")
+    cwd_dir = os.getcwd()
+    fileList = {}
+    sameGroupSim = []
+    differentGroupSim = []
+
+    dirList = os.listdir(path)
+    random.shuffle(dirList)
+    dirList = dirList[:20]
+
+    # same group compare
+    for d in dirList:
+        print("Processing Group %s self comparison" % d)
+        for root, dirs, files in os.walk(os.path.join(path, d)):
+            # 随机选取12个两两比较，计算平均相似度
+            random.shuffle(files)
+            files = files[0:12]
+            sim = []
+            for index, file in enumerate(files[:len(files) - 1]):
+                # f = open(os.path.join(root, file), "r", encoding='ISO-8859-1')
+                try:
+                    v1 = parser.parseCode(os.path.join(cwd_dir, root, file))
+                except pycparser.plyparser.ParseError:
+                    print("Cannot Open File %s" % os.path.join(cwd_dir, root, file))
+                    continue
+                # f.close()
+                for nextFile in files[index + 1:]:
+                    # f = open(os.path.join(root, nextFile), "r", encoding='ISO-8859-1')
+                    try:
+                        v2 = parser.parseCode(os.path.join(cwd_dir, root, nextFile))
+                    except pycparser.plyparser.ParseError:
+                        print("Cannot Open File %s" % os.path.join(cwd_dir, root, nextFile))
+                        continue
+                    com = np.array(parser.compareAll(v1, v2)).mean()  # 取平均
+                    sameGroupSim.append(com)
+
+    # different group
+    for root, dirs, _ in os.walk(path):
+        random.shuffle(dirs)
+        dirs = dirs[:6]
+        for d in dirs:
+            for _, _, files in os.walk(os.path.join(cwd_dir, root, d)):
+                random.shuffle(files)
+                files = files[0:10]
+                fileList[d] = files
+
+    visited = set()
+    for (d1, files1) in fileList.items():
+        for (d2, files2) in fileList.items():
+            print("Processing Group %s - Group %s" % (d1, d2))
+            if (d1, d2) in visited or d1 == d2:
+                continue
+            visited.add((d1, d2))
+            visited.add((d2, d1))
+            sim = []
+            for f1 in files1:
+                for f2 in files2:
+                    # f = open(os.path.join(path, d1, f1), "r", encoding='ISO-8859-1')
+                    try:
+                        v1 = parser.parseCode(os.path.join(cwd_dir, path, d1, f1))
+                    except pycparser.plyparser.ParseError:
+                        print("Cannot Open File %s" % os.path.join(cwd_dir, path, d1, f1))
+                        continue
+
+                    # f = open(os.path.join(path, d2, f2), "r", encoding='ISO-8859-1')
+                    try:
+                        # code = f.read()
+                        v2 = parser.parseCode(os.path.join(cwd_dir, path, d2, f2))
+                    except pycparser.plyparser.ParseError:
+                        print("Cannot Open File %s" % os.path.join(cwd_dir, path, d2, f2))
+                        continue
+                    # f.close()
+                    com = np.array(parser.compareAll(v1, v2)).mean()
+                    differentGroupSim.append(com)
+    return sameGroupSim, differentGroupSim
 
 
 def getSimilarityGroup():
@@ -127,7 +208,7 @@ def similarity_roc(sameGroupSim, differentGroupSim, show_hist=False, show_roc=Fa
 
 
 if __name__ == '__main__':
-    sameGroupSim, differentGroupSim = getSimilarityGroup()
+    sameGroupSim, differentGroupSim = getCppSimilarityGroup()
     with open("sim.log", "w") as f:
         f.writelines(str(sameGroupSim))
         f.writelines(str(differentGroupSim))
